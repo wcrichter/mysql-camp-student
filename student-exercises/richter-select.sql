ebdbcad9598a2d82cbf9372ad3b94494d3cb9f06
Chris Richter <wcrichter@gmail.com>
2017-03-14 11:49:52 -0400

    updated richter-select.sql with new SELECT queries

--- student-exercises/richter-select.sql
+++ student-exercises/richter-select.sql
@@ -6,6 +6,20 @@ SELECT * FROM individual;
 SELECT * FROM band;
 */
 
-SHOW COLUMNS FROM band;
+-- SHOW COLUMNS FROM band;
 
-SELECT ID, name, genre FROM band;
+-- SELECT ID, name, genre FROM band;
+
+SELECT * FROM individual
+WHERE LastName = 'Jennings';
+
+SELECT ID, LastName
+FROM individual
+WHERE DeceasedDate IS NOT NULL;
+
+SELECT ID, LastName, BirthDate
+FROM individual
+WHERE Year(BirthDate) > 1940;
+
+SELECT * FROM individual 
+WHERE ID IN (1,3,5,7,19);
