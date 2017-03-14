899db41d5d6b6434ed025d859db9c778a04b6d98
Chris Richter <wcrichter@gmail.com>
2017-03-14 13:27:13 -0400

    updated richter-select.sql with Exercise SELECT queries and added other exercise number comments

--- student-exercises/richter-select.sql
+++ student-exercises/richter-select.sql
@@ -6,10 +6,12 @@ SELECT * FROM individual;
 SELECT * FROM band;
 */
 
+-- Ex2
 -- SHOW COLUMNS FROM band;
-
 -- SELECT ID, name, genre FROM band;
 
+-- Ex3
+/*
 SELECT * FROM individual
 WHERE LastName = 'Jennings';
 
@@ -23,3 +25,12 @@ WHERE Year(BirthDate) > 1940;
 
 SELECT * FROM individual 
 WHERE ID IN (1,3,5,7,19);
+*/
+
+-- Ex4
+SELECT * FROM band
+WHERE isTogether = 1;
+
+SELECT ID, name
+FROM band
+WHERE ID = 4;
